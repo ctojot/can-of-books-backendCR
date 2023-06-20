e365a446ba96d7e2622663afa8db09ba5a58840d
ChristianRhey Tojot <christianrheytojot@gmail.com>
2023-06-19 17:02:23 -0700

    finished lab11 backend

---
+++ seed.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+mongoose.connect(process.env.DB_URL);
+
+const Book = require('./models/books.js');
+
+async function seed() {
+
+    await Book.create({
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        description: 'The first novel in the Harry Potter series and Rowling\'s debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.',
+        status: false
+    });
+    console.log('Harry Potter and the Sorcerer\'s Stone was created');
+
+    await Book.create({
+        title: 'The Perks of Being a Wallflower',
+        description: 'The Perks of Being a Wallflower is a 1999 book by Stephen Chbosky. It follows Charlie during his freshman year of high school as he makes and loses friends, experiments with drugs and his sexuality, and comes to grips with his past.',
+        status: false
+    });
+    console.log('The Perks of Being a Wallflower was created');
+
+    await Book.create({
+        title: 'The Absolutely True Diary of a Part-Time Indian',
+        description: 'The Absolutely True Diary of a Part-Time Indian is a first-person narrative novel by Sherman Alexie, from the perspective of a Native American teenager, Arnold Spirit Jr., also known as "Junior," a 14-year-old promising cartoonist.',
+        status: true
+    });
+    console.log('The Absolutely True Diary of a Part-Time Indian was created');
+
+    mongoose.disconnect();
+}
+
+seed(); 
